49ea11928a4f715b05579478cef16a928b6e1b04
Rafał Huk <r.huk@outlook.com>
2020-10-09 08:27:36 +0200

    Podpięcie przycisku wylogowania

--- Client/src/app/dashboard/main-page/main-page.component.ts
+++ Client/src/app/dashboard/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    console.log("Wylogowuję")
+    this.authService.logout();
   }
 
 }
